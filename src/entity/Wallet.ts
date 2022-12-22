f69b9b811451186cba95fae2352826a38a7481c3
GyuBa <sansam1010@naver.com>
2022-12-22 21:57:12 +0900

    First Init

---
+++ src/entity/Wallet.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class Wallet {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+
+    @Column()
+    age: number
+
+}
+
+
+
